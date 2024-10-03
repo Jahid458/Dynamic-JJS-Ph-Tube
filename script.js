2e96049a66237e456e15d43965462c3d07724d62
Jahid458 <jahidul898@outlook.com>
2024-10-03 11:06:51 +0600

    create dynamic category section

--- script.js
+++ script.js
@@ -4,15 +4,33 @@
 const loadCatagories = () =>{
       fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
       .then(res => res.json())
-      .then(data => displayCatagories(data.categories
-      ))
+      .then(data => displayCatagories(data.categories))
       .catch(error => console.log(error))
 
 
 }
+
+
+// {
+//   "category_id": "1001",
+//   "category": "Music"
+// }
 //create displayCatagories
-const displayCatagories = (data) =>{
-  console.log(data)
+const displayCatagories = (categories) =>{
+  const categoryContainer = document.getElementById("categories");
+  categories.forEach( item => {
+      //Create a button 
+      console.log(item)
+      const button = document.createElement("button");
+      button.classList = "btn";
+      button.innerText = item.category;
+
+      //add button to category container
+      categoryContainer.append(button);
+      
+      
+
+  });
 }
 
 loadCatagories()
