ad51f494ae6f65d7c7415d547332509ab955e059
Jahid458 <jahidul898@outlook.com>
2024-10-03 14:45:13 +0600

    string time convert

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-// fetch load catagori html
 
+function getTimeString(time){
+  //get hour and rest second
+  const hour =parseInt(time / 3600); 
+  let remainingSecond = time % 3600;
+  const miniute = parseInt(remainingSecond /60);
+  remainingSecond =  remainingSecond % 60 ;
+  return `${hour} hour ${miniute} miniute ${remainingSecond} second ago` ;
+}
+// fetch load catagori html
 //create loadcatagories
 const loadCatagories = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
@@ -38,24 +46,35 @@ const displayVideos = (videos) => {
   videos.forEach((video) => {
     console.log(video);
     const card = document.createElement("div");
-    card.classList = "card card-compact"
+    card.classList = "card card-compact";
     card.innerHTML = `
          <figure class="h-[200px] relative">
     <img 
       src="${video.thumbnail}"
       class = "h-full w-full object-cover"
       alt="Shoes" />
-      <span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${video.others.posted_date}</span>
+      ${
+        video.others.posted_date?.length == 0
+          ? ""
+          : `<span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${getTimeString(video.others.posted_date)}</span> `
+      }
+      
   </figure>
   <div class="px-0 py-2 flex gap-2">
         <div>
-            <img class="w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}" />
+            <img class="w-10 h-10 rounded-full object-cover" src="${
+              video.authors[0].profile_picture
+            }" />
         </div>
         <div >
            <h2 class="font-bold">${video.title}</h2>
            <div class="flex items-center gap-2">
               <p class="text-gray-400">${video.authors[0].profile_name}</p>
-                ${video.authors[0].verified == true ? '<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png">': ""}
+                ${
+                  video.authors[0].verified == true
+                    ? '<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png">'
+                    : ""
+                }
               
            </div>
            <p></p>
@@ -66,7 +85,7 @@ const displayVideos = (videos) => {
    
   </div>
         `;
-      videoContainer.append(card)
+    videoContainer.append(card);
   });
 };
 
---
+++ text.js
@@ -0,0 +1,12 @@
+
+
+// function getTimeString(time){
+//   //get hour and rest second
+//   const hour =parseInt(time / 3600); 
+//   let remainingSecond = time % 3600;
+//   const miniute = parseInt(remainingSecond /60);
+//   remainingSecond =  remainingSecond % 60 ;
+//   return `${hour} hour ${miniute} miniute ${remainingSecond} second ago` ;
+// }
+
+console.log(getTimeString(7865))
